0c1ae3c3f7ba20dfc205a7c69d2db496c710c3c2
maxisoft <maxisoft.eu@gmail.com>
2022-04-26 10:12:30 +0200

    scripts/compress_databases.py now set journal mode to delete before compression. Added support for other zip's compression algo

--- scripts/compress_databases.py
+++ scripts/compress_databases.py
@@ -4,12 +4,14 @@ This script compress oldest bitfinex databases into multiple zip files.
 Usage: ./compress_databases.py path/to/databases
 """
 from asyncio import as_completed
+import importlib
 import re
 import sys
 import os
 import zipfile
 import logging
 import time
+import sqlite3
 
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -17,18 +19,33 @@ from concurrent.futures import ThreadPoolExecutor, Future, as_completed
 
 from typing import Any, Callable, Collection, List, Union
 
+from injector import T
+
 logger = logging.getLogger("bitfinex.databases.compress")
 
+
+def _compression_mode():
+    if os.environ.get("COMPRESS_DEFLATE", False) not in ('', '0', 'false', 'no', False, None):
+        return zipfile.ZIP_DEFLATED
+    if os.environ.get("COMPRESS_BZIP2", False) not in ('', '0', 'false', 'no', False, None):
+        return zipfile.ZIP_BZIP2
+
+    try:
+        lzma = bool(importlib.util.find_spec('lzma'))
+    except ImportError:
+        lzma = False
+
+    return zipfile.ZIP_LZMA if lzma else zipfile.ZIP_DEFLATED
 
 def compress_file(p: Path):
     assert p.is_file()
     with zipfile.ZipFile(
-        str(p) + ".zip", "w", compression=zipfile.ZIP_LZMA
+        str(p) + ".zip", "w", compression=_compression_mode()
     ) as out:
         out.write(str(p.resolve()), p.name)
 
 
-def list_file(p: Path, patterns=("**/*.sqlite", "**/*.sqlite-wal", "**/*.sqlite-shm")):
+def list_file(p: Path, patterns=("**/*.sqlite",)):
     deja_vu = set()
     for pattern in patterns:
         for sub in p.glob(pattern):
@@ -54,8 +71,27 @@ def filter_file_time(p: Path):
     return False
 
 
+def _set_journal_mode(db_path: Path, journal_mode='DELETE'):
+    stat = db_path.stat()
+    try:
+        uri = f'{db_path.resolve().as_uri()}?mode=ro'
+        with sqlite3.connect(uri, uri=True) as db:
+            current_mode = db.execute('''PRAGMA journal_mode''')
+            current_mode = current_mode.fetchone()[0]
+        if current_mode.upper() == journal_mode.upper():
+            return
+        try:
+            with sqlite3.connect(str(db_path), uri=True) as db:
+                db.execute(f'''PRAGMA journal_mode={journal_mode};''')
+        except:
+            pass
+    finally:
+        if db_path.stat().st_mtime != stat.st_mtime:
+            os.utime(str(db_path), (stat.st_atime, stat.st_mtime))
+
 def _compress_task_fn(path):
     print(f"compressing {path} ...")
+    _set_journal_mode(path)
     compress_file(path)
     return path
 
@@ -71,7 +107,7 @@ def _walk(path: Path, collect_fn: Callable[[Path], Any]):
     elif filter_file_time(path):
         yield collect_fn(path)
     return tuple()
-
+    
 
 def main(*paths: Collection[Union[str, Path]]):
     with ThreadPoolExecutor(
@@ -104,7 +140,6 @@ def main(*paths: Collection[Union[str, Path]]):
                 task.cancel()
             executor.shutdown(wait=False)
             raise
-            
 
 
 if __name__ == "__main__":
