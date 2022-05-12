50da51b3aa92bb59850a68f60fe4ddb07139c126
maxisoft <maxisoft.eu@gmail.com>
2022-05-12 14:16:13 +0200

    Docker startup script: hide unneeded commands outputs

--- docker/start_bitfinex_dumper.sh
+++ docker/start_bitfinex_dumper.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 PUID=${PUID:-912}
-usermod -u "$PUID" "${BITFINEX_USER_NAME:-bitfinexdumper}"
+usermod -u "$PUID" "${BITFINEX_USER_NAME:-bitfinexdumper}" >/dev/null
 PGID=${PGID:-912}
-groupmod -g "$PGID" "${BITFINEX_GROUP_NAME:-bitfinexdumper}"
+groupmod -g "$PGID" "${BITFINEX_GROUP_NAME:-bitfinexdumper}" >/dev/null
 
 BITFINEX_DEST_FOLDER=${BITFINEX_DEST_FOLDER:-/bitfinexdumper}
 # create folders
@@ -18,5 +18,5 @@ if [ ! "$(stat -c %u "${BITFINEX_DEST_FOLDER}")" = "$PUID" ]; then
 	chown "$PUID:$PGID" -R "${BITFINEX_DEST_FOLDER}"
 fi
 
-renice "+${NICE_ADJUSTEMENT:-1}" $$ || :
+renice "+${NICE_ADJUSTEMENT:-1}" $$ >/dev/null 2>&1 || :
 exec ionice -c "${IONICE_CLASS:-3}" -n "${IONICE_CLASSDATA:-7}" -t su-exec "$PUID:$PGID" "${BITFINEX_DUMPER_EXE:-bitfinex-dumper}" $@
